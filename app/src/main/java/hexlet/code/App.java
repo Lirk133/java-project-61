60a86d4ae8c9bd5bbbdc1442e5d3f81991627c2b
Lirk133 <lirk133@gmail.com>
2025-04-13 21:29:28 +0300

    fix App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -10,8 +10,7 @@ public class App {
     public static void main(String[] args) {
 
         //основное меню выбора игры
-        while (true) {
-            System.out.print("""
+        System.out.print("""
                     Please enter the game number and press Enter.
                     1 - Greet
                     2 - Even
@@ -22,35 +21,30 @@ public class App {
                     0 - Exit
                     Your choice:\s""");
 
-            String choiceGame = Engine.scannerString();
+        String choiceGame = Engine.scannerString();
 
-            switch (choiceGame) {
-                case "1":
-                    Cli.startGame();
-                    break;
-                case "2":
-                    Even.gameEven();
-                    break;
-                case "3":
-                    Calc.gameCalc();
-                    break;
-                case "4":
-                    GCD.gameGCD();
-                    break;
-                case "5":
-                    Progression.gameProgression();
-                    break;
-                case "6":
-                    Prime.gamePrime();
-                    break;
-                case "0":
-                    System.exit(0);
-                    break;
-                default:
-                    System.out.println("Wrong command.\nPlease try again.");
-                    break;
-            }
+        switch (choiceGame) {
+            case "1":
+                Cli.startGame();
+                break;
+            case "2":
+                Even.gameEven();
+                break;
+            case "3":
+                Calc.gameCalc();
+                break;
+            case "4":
+                GCD.gameGCD();
+                break;
+            case "5":
+                Progression.gameProgression();
+                break;
+            case "6":
+                Prime.gamePrime();
+                break;
+            default:
+                System.exit(0);
+                break;
         }
-
     }
 }
