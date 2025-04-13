6ae6b57d0cfec3f2175cf53f7a9f7b88fc606a3a
Lirk133 <lirk133@gmail.com>
2025-04-13 11:51:07 +0300

    lesson 3

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,14 @@
 package hexlet.code;
 
+
 public class App {
     public static void main(String[] args) {
+
         System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+
+        String nameGamer = Cli.scannerString();
+
+        System.out.println("Hello, " + nameGamer + "!");
     }
 }
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,8 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    static public String scannerString () {
+        return new Scanner(System.in).nextLine();
+    }
+}
